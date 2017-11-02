2b671264491b465e96a6f0834406da1e55b496fb
Joshua D <joshdecosta@gmail.com>
2017-11-01 20:18:03 -0400

    added various statistics related to the data set

--- import_data.py
+++ import_data.py
@@ -13,3 +13,17 @@ prices = data["AV_TOTAL"];
 features = data.drop('AV_TOTAL', axis=1);
 
 print("Boston housing dataset has {0} data points and {1} variables each".format(*data.shape))
+
+minimum_price = np.min(prices);
+max_price = np.max(prices);
+mean_price = np.mean(prices);
+median_price = np.median(prices);
+std_price = np.std(prices);
+
+
+print("various values contained within our Boston data set");
+print("minimum price in the data set:${:,.2f}".format(minimum_price));
+print("max price in the data set:${:,.2f}".format(max_price));
+print("mean price in the data set:${:,.2f}".format(mean_price));
+print("median price in the data set:${:,.2f}".format(median_price));
+print("standard deviation of the data set:${:,.2f}".format(std_price));
