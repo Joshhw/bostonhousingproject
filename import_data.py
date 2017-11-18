bd83a63a0dc6ec5a2c4bd990704eb16acd6a8673
Joshua D <joshdecosta@gmail.com>
2017-11-18 16:10:54 -0500

    modified columns to keep as feature set
    What started out as 63 potential columns has very little relevance
    for anything condo related. I decided to remove all string data since
    our linear regression won't play well with it.

--- import_data.py
+++ import_data.py
@@ -6,13 +6,26 @@ from sklearn.cross_validation import ShuffleSplit
 #import all data sets aka 2016 data set
 data = pd.read_csv('data-sets/Parcels_2016_Data_Full.csv',header=0);
 
+#columns to keep
+keep_col = [
+    "PID", "ZIPCODE", "GROSS_AREA", 
+    "LIVING_AREA", "GROSS_TAX", "LAND_SF",
+    "YR_BUILT", "YR_REMOD", "NUM_FLOORS",
+    "U_TOT_RMS", "U_BDRMS", "U_FULL_BTH",
+    "U_HALF_BTH", "U_FPLACE", "SHAPESTArea", 
+    "SHAPESTLength" 
+    ];
+
+#drop anything that isn't a condo
+data = data[pd.notnull(data['U_TOT_RMS'])];
+
 #probs the actual price
 prices = data["AV_TOTAL"];
 
 #potential features
-features = data.drop('AV_TOTAL', axis=1);
+features = data[keep_col];
 
-print("Boston housing dataset has {0} data points and {1} variables each".format(*data.shape))
+print("Boston housing dataset has {0} data points and {1} variables each".format(*features.shape))
 
 minimum_price = np.min(prices);
 max_price = np.max(prices);
@@ -27,3 +40,4 @@ print("max price in the data set:${:,.2f}".format(max_price));
 print("mean price in the data set:${:,.2f}".format(mean_price));
 print("median price in the data set:${:,.2f}".format(median_price));
 print("standard deviation of the data set:${:,.2f}".format(std_price));
+
